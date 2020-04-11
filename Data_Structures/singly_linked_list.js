83611122d524aa9360412bf488b263d89540dbed
Mitja <mitjasusec@outlook.com>
2020-04-11 21:10:10 +0200

    11.4.2020

---
+++ Data_Structures/singly_linked_list.js
@@ -0,0 +1,34 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class Linked_List {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return newNode;
+  }
+
+  pop() {
+    
+
+
+
+  }
+}
---
+++ Problem_Solving_Patterns/sliding_window.js
@@ -0,0 +1,7 @@
+// Sliding window pattern involves creating a window which can either be an array or number from one position to another.
+// Depending on a certain condition, window increases or closes. Useful for keeping track of subset of data in array / string etc.
+
+// Write a function maxSubarraySum which accepts an array of integers and number n and calculates maximum sum of n consecutive elements in the array
+
+function maxSubarraySum(arr, n) {    
+}
